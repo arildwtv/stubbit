414a506ff0fcdce0ff083d8a4d299c4831f7c9a4
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-02-19 09:10:14 +0100

    Fix bug where relative paths to dependencies were resolved incorrectly.

--- test/stubbit.test.js
+++ test/stubbit.test.js
@@ -4,35 +4,35 @@ var assert = require('assert');
 describe('stubbit', function () {
 
     describe('requireWithStubs', function () {
-        it('does not stubbit modules that are not explicitly declared', function () {
+        it('does not stub dependencies that are not explicitly declared', function () {
             var foo = stubbit.requireWithStubs('./resources/foo');
             assert.equal('foobar', foo.foo());
         });
 
-        it('stubs explicitly declared modules using empty stubs', function () {
-            var foo = stubbit.requireWithStubs('./resources/foo', './resources/bar');
+        it('stubs explicitly declared dependencies using empty stubs', function () {
+            var foo = stubbit.requireWithStubs('./resources/foo', './bar');
 
             assert.equal('fooundefined', foo.foo());
         });
 
         it('throws error if module does not exist', function () {
             assert.throws(function () {
-                var foo = stubbit.requireWithStubs('./resources/foo2', './resources/bar');
+                var foo = stubbit.requireWithStubs('./resources/foo2', './bar');
             });
         });
 
         it('throws error if dependency does not exist', function () {
             assert.throws(function () {
-                var foo = stubbit.requireWithStubs('./resources/foo', './resources/bar2');
+                var foo = stubbit.requireWithStubs('./resources/foo', './bar2');
             });
         });
 
         it('throws error if dependency is neither an object or a function', function () {
             assert.throws(
                 function () {
-                    var foo = stubbit.requireWithStubs('./resources/foo', './resources/anumber');
+                    var foo = stubbit.requireWithStubs('./resources/foo', './anumber');
                 },
-                /Dependency \.\/resources\/anumber of type number is not supported/
+                /Dependency \.\/anumber of type number is not supported/
             );
         });
     });
@@ -41,48 +41,49 @@ describe('stubbit', function () {
         it('throws error if module does not exist', function () {
             assert.throws(function () {
                 stubbit.getStub('test');
-            });
+            },
+            /Module is not an object/);
         });
 
         it('throws error if stubbed dependency does not exist', function () {
-            var foo = stubbit.requireWithStubs('./resources/foo', './resources/bar');
+            var foo = stubbit.requireWithStubs('./resources/foo', './bar');
 
             assert.throws(function () {
                 stubbit.getStub(foo, './does-not-exist');
             },
-            /Stubbed dependency does not exist/);
+            /Stubbed dependency \.\/does-not-exist does not exist/);
         });
 
         it('throws error if stubbed dependency does not have the provided method', function () {
-            var foo = stubbit.requireWithStubs('./resources/foo', './resources/bar');
+            var foo = stubbit.requireWithStubs('./resources/foo', './bar');
 
             assert.throws(function () {
-                stubbit.getStub(foo, './resources/bar', 'unknownMethod');
+                stubbit.getStub(foo, './bar', 'unknownMethod');
             },
-            /Stubbed dependency does not have the function unknownMethod/);
+            /Stubbed dependency \.\/bar does not have the function unknownMethod/);
         });
 
         it('returns the entire stubbed dependency if no method argument is provided', function () {
-            var foo = stubbit.requireWithStubs('./resources/foo', './resources/bar');
+            var foo = stubbit.requireWithStubs('./resources/foo', './bar');
 
-            var stub = stubbit.getStub(foo, './resources/bar');
+            var stub = stubbit.getStub(foo, './bar');
 
             assert.ok(typeof stub === 'object');
         });
 
         it('returns the stubbed method if method argument is provided', function () {
-            var foo = stubbit.requireWithStubs('./resources/foo', './resources/bar');
+            var foo = stubbit.requireWithStubs('./resources/foo', './bar');
 
-            var stubbedMethod = stubbit.getStub(foo, './resources/bar', 'bar');
+            var stubbedMethod = stubbit.getStub(foo, './bar', 'bar');
 
             assert.ok(stubbedMethod && stubbedMethod.id && stubbedMethod.id.match(/^spy/));
         });
 
         it('returns the stubbed method which exposes Sinon\'s API', function () {
-            var foo = stubbit.requireWithStubs('./resources/foo', './resources/bar');
+            var foo = stubbit.requireWithStubs('./resources/foo', './bar');
 
             // Test Sinon's returns API.
-            stubbit.getStub(foo, './resources/bar', 'bar').returns('baz');
+            stubbit.getStub(foo, './bar', 'bar').returns('baz');
 
             assert.equal('foobaz', foo.foo());
         });
